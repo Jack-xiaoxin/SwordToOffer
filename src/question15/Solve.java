3d11c6fd03a31569652a7797499d6d29a70d3bc8
陈鑫 <915013255@qq.com>
2021-07-12 10:42:12 +0800

    15题修改

--- src/question15/Solve.java
+++ src/question15/Solve.java
@@ -3,6 +3,8 @@ package question15;
 public class Solve {
 
     public ListNode deleteNode(ListNode head, ListNode delete) {
+        if(head == null || delete == null) return null;
+        if(delete.next == null) delete = null;
         ListNode temp = delete.next;
         delete.val = temp.val;
         delete.next = temp.next;
