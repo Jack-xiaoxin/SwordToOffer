121da3ce38790b688ba710b8000c5f75f808e566
陈鑫 <915013255@qq.com>
2021-07-27 00:02:08 +0800

    38题，1-中1出现的次数

--- src/question38/Solve.java
+++ src/question38/Solve.java
@@ -3,7 +3,7 @@ package question38;
 public class Solve {
 
     public static void main(String[] args) {
-        System.out.println(new Solve().countDigitOne(13));
+        System.out.println(new Solve().countDigitOne(100));
     }
 
     public int countDigitOne(int n) {
@@ -16,7 +16,17 @@ public class Solve {
     }
 
     public int help(String left, String cur, String right) {
-
+        int count = 0;
+        int curVal = Integer.parseInt(cur);
+        if(left.length() == 0 && right.length() == 0) count = curVal >= 1 ? 1 : 0;
+        else if(left.length() == 0) count = curVal > 1 ? (int)Math.pow(10, right.length()) : Integer.parseInt(right)+1;
+        else if(right.length() == 0) count = curVal >= 1 ? Integer.parseInt(left)+1 : Integer.parseInt(left);
+        else {
+            count += Integer.parseInt(left) * (int)Math.pow(10, right.length());
+            if(curVal == 1) count += Integer.parseInt(right) + 1;
+            else if(curVal > 1) count += (int)Math.pow(10, right.length());
+        }
+        return count;
     }
 
 }
