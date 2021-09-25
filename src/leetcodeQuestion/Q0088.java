16f750c57b11810b090d1674957237561b34f074
陈鑫 <915013255@qq.com>
2021-09-25 12:35:09 +0800

    增加了第88题，合并两个有序数组

---
+++ src/leetcodeQuestion/Q0088.java
@@ -0,0 +1,15 @@
+package leetcodeQuestion;
+
+public class Q0088 {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = nums1.length-1;
+        int i = m-1, j = n-1;
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) nums1[index--] = nums1[i--];
+            else nums1[index--] = nums2[j--];
+        }
+        while(i >= 0) nums1[index--] = nums1[i--];
+        while(j >= 0) nums1[index--] = nums2[j--];
+    }
+}
