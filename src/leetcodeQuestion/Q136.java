9be0170acb1ba72de1ab19ef371565536cdcf085
陈鑫 <915013255@qq.com>
2021-09-27 14:28:49 +0800

    增加了第136题，只出现了一次的数字1

---
+++ src/leetcodeQuestion/Q136.java
@@ -0,0 +1,12 @@
+package leetcodeQuestion;
+
+public class Q136 {
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
